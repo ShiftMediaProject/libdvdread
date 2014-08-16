1cf55e06a04e351dfe6800ac5b4ca2d34633d6d0
Matthew Oliver <protogonoi@gmail.com>
2014-08-16 13:52:58 +1000

    Add VS project files.

---
+++ SMC/version.h
@@ -0,0 +1,9 @@
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libdvdread 5.0.0"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "5.0.0"
+
+/* Version number of package */
+#define VERSION "5.0.0"

874873d654fc523a2fa45af2abe2cf69599412af
Jean-Baptiste Kempf <jb@videolan.org>
2014-08-28 08:50:29 +0200

    Force gcc-style packing

--- src/dvdread/ifo_types.h
+++ src/dvdread/ifo_types.h
@@ -32,7 +32,7 @@
 
 #if defined(__GNUC__)
 #if __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 95)
-#define ATTRIBUTE_PACKED __attribute__ ((packed))
+#define ATTRIBUTE_PACKED __attribute__ ((packed,gcc_struct))
 #define PRAGMA_PACK 0
 #endif
 #endif

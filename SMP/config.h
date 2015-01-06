8258f5a36ca5d59f66586af7dd5c2aa7b185836d
Matt Oliver <protogonoi@gmail.com>
2015-01-06 16:44:12 +1100

    project: Update for recent merge.

--- SMP/config.h
+++ SMP/config.h
@@ -1,11 +1,6 @@
-/* config.h.  Generated from config.h.in by configure.  */
-/* config.h.in.  Generated from configure.ac by autoheader.  */
 
-/* Define if building universal (internal helper macro) */
-/* #undef AC_APPLE_UNIVERSAL_BUILD */
-
-/* Define to 1 if you have the <dlfcn.h> header file. */
-/* #undef HAVE_DLFCN_H */
+/* Unused parameter annotation. */
+#define UNUSED
 
 /* Define to 1 if you have the <dvdcss/dvdcss.h> header file. */
 #ifndef NO_DVDCSS_LINK
@@ -48,52 +43,5 @@
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
 
-/* Define to the sub-directory in which libtool stores uninstalled libraries.
-   */
-#define LT_OBJDIR ".libs/"
-
-/* Name of package */
-#define PACKAGE "libdvdread"
-
-/* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT ""
-
-/* Define to the full name of this package. */
-#define PACKAGE_NAME "libdvdread"
-
-/* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "libdvdread"
-
-/* Define to the home page for this package. */
-#define PACKAGE_URL ""
-
-#include "version.h"
-
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
-
-/* "Define to 1 to use builtin dlfcn" */
-/* #undef USING_BUILTIN_DLFCN */
-
-/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
-   significant byte first (like Motorola and SPARC, unlike Intel). */
-#if defined AC_APPLE_UNIVERSAL_BUILD
-# if defined __BIG_ENDIAN__
-#  define WORDS_BIGENDIAN 1
-# endif
-#else
-# ifndef WORDS_BIGENDIAN
-/* #  undef WORDS_BIGENDIAN */
-# endif
-#endif
-
-/* Enable large inode numbers on Mac OS X 10.5.  */
-#ifndef _DARWIN_USE_64_BIT_INODE
-# define _DARWIN_USE_64_BIT_INODE 1
-#endif
-
-/* Number of bits in a file offset, on hosts where this is settable. */
-#define _FILE_OFFSET_BITS 64
-
-/* Define for large files, on AIX-style hosts. */
-/* #undef _LARGE_FILES */
--- SMP/version.h
+++
@@ -1,9 +0,0 @@
-
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libdvdread 5.0.0"
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "5.0.0"
-
-/* Version number of package */
-#define VERSION "5.0.0"

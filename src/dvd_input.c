1c031048907b72c4757926a06f2bc5fac42fba15
Brad Smith <brad@comstyle.com>
2021-09-11 20:57:58 -0400

    Use correct name of the libdvdcss shared library on OpenBSD
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- src/dvd_input.c
+++ src/dvd_input.c
@@ -346,6 +346,8 @@ int dvdinput_setup(void *priv, dvd_logger_cb *logcb)
   #define CSS_LIB "libdvdcss-2.dll"
 #elif defined(__OS2__)
   #define CSS_LIB "dvdcss2.dll"
+#elif defined(__OpenBSD__)
+  #define CSS_LIB "libdvdcss.so"
 #else
   #define CSS_LIB "libdvdcss.so.2"
 #endif
